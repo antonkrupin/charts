59005be1fb6be0f67c3749374d6b980196bd6420
Anton Krupin <krupin_anton@mail.ru>
2022-10-04 20:26:07 +0300

    change handleSomething

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
+import { addChart, updateChart, deleteChart } from '../slices/chartReducer';
+
 const Chart = (props) => {
-	const handleSomething = (e) => {
-		console.log(props.id);
+	// const charts = useSelector((state) => state.chart.charts);
+	const dispatch = useDispatch();
+
+	const handleSomething = () => {
+		dispatch(deleteChart(props.id));
 	}
 	return (
 		<div className="flex-column m-1 border border-primary rounded">
 			<HighchartsReact highcharts={Highcharts} options={props.options} />
-			<Button onClick={handleSomething} className="m-2" variant="info">Change</Button>
+			<Button className="m-2" variant="info">Change</Button>
 			<Button onClick={handleSomething} className="m-2" variant="danger">Delete</Button>
 		</div>
 	)
