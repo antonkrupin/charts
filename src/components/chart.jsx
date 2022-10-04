ebd16f08709dd78c85e7faa91836fa6bd6d1bf63
Anton Krupin <krupin_anton@mail.ru>
2022-10-04 20:04:53 +0300

    change chart.jsx

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -4,12 +4,14 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 const Chart = (props) => {
-	
+	const handleSomething = (e) => {
+		console.log(props.id);
+	}
 	return (
 		<div className="flex-column m-1 border border-primary rounded">
 			<HighchartsReact highcharts={Highcharts} options={props.options} />
-			<Button className="m-2" variant="info">Change</Button>
-			<Button className="m-2" variant="danger">Delete</Button>
+			<Button onClick={handleSomething} className="m-2" variant="info">Change</Button>
+			<Button onClick={handleSomething} className="m-2" variant="danger">Delete</Button>
 		</div>
 	)
 }
