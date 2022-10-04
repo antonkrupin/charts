6509ccecd1767c7c2ce762066c2594310fe75a5b
antonkrupin <krupin_anton@mail.ru>
2022-10-04 14:06:20 +0300

    change addChart reducer

--- src/slices/chartReducer.js
+++ src/slices/chartReducer.js
@@ -1,15 +1,43 @@
+import _ from 'lodash';
 import { createSlice } from '@reduxjs/toolkit';
+import { charts } from 'highcharts';
 
 const initialState = {
-	charts: {},
+	charts: [],
 }
 
 const chartSlice = createSlice({
 	name: 'chart',
 	initialState,
 	reducers: {
-		addChart: (state) => {
-
+		addChart: (state, action) => {
+			const id = _.uniqueId();
+			const { title, type } = action.payload;
+			state.charts.push({
+				id,
+				options: {
+					chart: {
+						type: type,
+					},
+					title: {
+						text: title,
+					},
+					series: [
+						{	
+							name: 'First',
+							data: [1, 2, 1, 4, 3, 6],
+						},
+						{
+							name: 'Second',
+							data: [2, 4, 5, 1, 8, 9],
+						},
+						{
+							name: 'Third',
+							data: [0, 10, 2, 1, 10, 12],
+						}
+					]
+				}
+			})
 		},
 		updateChart: (state) => {
 
@@ -23,3 +51,33 @@ const chartSlice = createSlice({
 export const { addChart, updateChart, deleteChart } = chartSlice.actions;
 
 export default chartSlice.reducer;
+
+/*
+
+const options = {
+		chart: {
+			type: 'spline'
+		},
+		title: {
+			text: 'My chart'
+		},
+		series: [
+			{	
+				name: 'First',
+				data: [1, 2, 1, 4, 3, 6],
+			},
+			{
+				name: 'Second',
+				data: [2, 4, 5, 1, 8, 9],
+			},
+			{
+				name: 'Third',
+				data: [0, 10, 2, 1, 10, 12],
+			}
+		]
+	};
+
+	charts = [{id, type, title}],
+	chartsData = [{id, lines: {name, data:[]}}]
+
+*/
