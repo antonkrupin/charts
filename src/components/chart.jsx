991f2497d86d34a51cc7e06354732cd05e4cb217
antonkrupin <krupin_anton@mail.ru>
2022-10-04 14:07:16 +0300

    remove const options

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -5,37 +5,11 @@ import { Button } from 'react-bootstrap';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
-const Chart = () => {
-	const options = {
-		chart: {
-			type: 'spline'
-		},
-		title: {
-			text: 'My chart'
-		},
-		series: [
-			{	
-				name: 'First',
-				data: [1, 2, 1, 4, 3, 6],
-			},
-			{
-				name: 'Second',
-				data: [2, 4, 5, 1, 8, 9],
-			},
-			{
-				name: 'Third',
-				data: [0, 10, 2, 1, 10, 12],
-			}
-		]
-	};
-
-	const charts = useSelector((state) => state.chart.charts);
-	const dispatch = useDispatch();
-	const location = useLocation();
+const Chart = (props) => {
 	
 	return (
 		<div className="flex-column m-1 border border-primary rounded">
-			<HighchartsReact highcharts={Highcharts} options={options} />
+			<HighchartsReact highcharts={Highcharts} options={props.options} />
 			<Button className="m-2" variant="info">Change</Button>
 			<Button className="m-2" variant="danger">Delete</Button>
 		</div>
