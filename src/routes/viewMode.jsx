38c2c091f090a71ae8b8776ee72500b3a69574bc
antonkrupin <krupin_anton@mail.ru>
2022-10-05 15:21:49 +0300

    change

--- src/routes/viewMode.jsx
+++ src/routes/viewMode.jsx
@@ -1,16 +1,30 @@
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 import Chart from '../components/chart';
 import AlertMessage from '../components/alert';
+import CreationDateFilter from '../components/creationDateFilter';
 
 const ViewMode = () => {
+	const location = useLocation().pathname;
+
 	const charts = useSelector((state) => state.chart.charts);
+
+	const chartsFilteredByDate = useSelector((state) => state.chart.chartsFilteredByDate);
+
+	let test;
+	if (chartsFilteredByDate.length === 0) {
+		test = charts;
+	} else {
+		test = chartsFilteredByDate;
+	}
 	
 	return (
 		<>
 		<AlertMessage />
+		<CreationDateFilter />
 		<div className="d-flex flex-wrap justify-content-center">
-			{ charts.map((chart) => <Chart key={chart.id} options={chart.options} />) }
+			{ test.map((chart) => <Chart key={chart.id} options={chart.options} location={location}/>) }
 		</div>
 		</>
 	)
