4e52b727ed59edd9756cab9a3c59620cf01371ae
antonkrupin <krupin_anton@mail.ru>
2022-10-06 16:13:00 +0300

    change text

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -1,12 +1,19 @@
+import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Alert } from 'react-bootstrap';
 
 const AlertMessage = () => {
 	const charts = useSelector((state) => state.chart.charts);
+	const location = useLocation();
 	
+	let alertText = 'You have no Charts. Go to Settings, to Add Chart';
+	if (location.pathname === '/settings') {
+		alertText = 'To add Chart - click button Add chart';
+	}
+
 	if (charts.length === 0) {
 		return ( <Alert className="text-center" variant="warning" >
-			You have no Charts. To add Chart - click button "Add chart"
+			{alertText}
 		</Alert>
 		)
 	}
