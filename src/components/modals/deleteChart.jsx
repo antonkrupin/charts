ede4fd8a029a186a7c5db8074589af7b57fbf33d
antonkrupin <krupin_anton@mail.ru>
2022-10-07 15:15:31 +0300

    change

--- src/components/modals/deleteChart.jsx
+++ src/components/modals/deleteChart.jsx
@@ -1,26 +1,27 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Modal, Button, Form, InputGroup } from 'react-bootstrap';
-import { changeDeleteModalShow, deleteChart } from '../../slices/chartReducer';
+import { Modal, Button } from 'react-bootstrap';
+import { deleteChart } from '../../slices/chartReducer';
+import { deleteChartModalShow } from '../../slices/modalsReducer';
 
 const DeleteModal = (props) => {
 	const dispatch = useDispatch();
   
-	const isDeleteModalShow = useSelector((state) => state.chart.isDeleteModalShow);
+	const isDeleteChartModalShow = useSelector((state) => state.modals.isDeleteChartModalShow);
 	
 	const handleDelete = () => {
 		console.log(props.id)
 		dispatch(deleteChart(props.id));
-		dispatch(changeDeleteModalShow());
+		dispatch(deleteChartModalShow(props.id));
 	}
 
 	return (
-    <Modal show={isDeleteModalShow} onHide={() => dispatch(changeDeleteModalShow())}>
+    <Modal show={isDeleteChartModalShow} onHide={() => dispatch(deleteChartModalShow())}>
       <Modal.Header closeButton>
         <Modal.Title>Delete Chart?</Modal.Title>
       </Modal.Header>
       <Modal.Footer>
-        <Button variant="info" onClick={() => dispatch(changeDeleteModalShow())}>
+        <Button variant="info" onClick={() => dispatch(deleteChartModalShow())}>
           No
         </Button>
         <Button variant="danger" onClick={handleDelete}>
