5e8013430f698dc932e60a198fcf8abc1b5d2aa7
antonkrupin <krupin_anton@mail.ru>
2022-10-07 15:14:23 +0300

    add chartReducer

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chartReducer from '../slices/chartReducer';
+import modalsReducer from '../slices/modalsReducer';
 
 export default configureStore({
 	reducer: {
 		chart: chartReducer,
+		modals: modalsReducer,
 	},
 });
