fdcfbb88f913906f5c6837e060ac89a2a655b54b
Anton Krupin <krupin_anton@mail.ru>
2022-10-08 13:25:53 +0300

    remove import { colorPicker }

--- src/routes/page404.jsx
+++ src/routes/page404.jsx
@@ -2,9 +2,11 @@ import { Alert } from 'react-bootstrap';
 
 const Page404 = () => {
 	return (
+		<>
 		<Alert className="text-center" variant="warning" >
 		This is Page 404. <br/>Something go wrong.<br /><strong>Use links above to get right page.</strong>
 	</Alert>
+	</>
 	)
 }
 
