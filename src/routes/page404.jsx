281d130e8364560c8d2d839f42a7e31610bbde48
Anton Krupin <krupin_anton@mail.ru>
2022-10-08 13:13:10 +0300

    change markup

--- src/routes/page404.jsx
+++ src/routes/page404.jsx
@@ -1,5 +1,11 @@
+import { Alert } from 'react-bootstrap';
+
 const Page404 = () => {
-	return <h2>Page404</h2>
+	return (
+		<Alert className="text-center" variant="warning" >
+		This is Page 404. <br/>Something go wrong.<br /><strong>Use links above to get right page.</strong>
+	</Alert>
+	)
 }
 
 export default Page404;
