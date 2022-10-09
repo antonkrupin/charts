7bd545719bcf0a05cdccb42d77cc4cf2ff39335e
Anton Krupin <krupin_anton@mail.ru>
2022-10-09 19:48:52 +0300

    add import React from 'react'

--- src/routes/page404.jsx
+++ src/routes/page404.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import { Alert } from 'react-bootstrap';
 
 const Page404 = () => {
 	return (
 		<>
-		<Alert className="text-center" variant="warning" >
-		This is Page 404. <br/>Something go wrong.<br /><strong>Use links above to get right page.</strong>
-	</Alert>
-	</>
+			<Alert className="text-center" variant="warning" >
+				This is Page 404. <br/>Something go wrong.<br /><strong>Use links above to get right page.</strong>
+			</Alert>
+		</>
 	)
 }
 
