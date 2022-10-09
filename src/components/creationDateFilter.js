e7de89ac1f070fe4485523acfbdbd64a87de52f4
Anton Krupin <krupin_anton@mail.ru>
2022-10-09 12:22:13 +0300

    change onChange handler

--- src/components/creationDateFilter.js
+++ src/components/creationDateFilter.js
@@ -5,14 +5,14 @@ import { Form } from 'react-bootstrap';
 import { filterChartsByDate } from '../slices/chartReducer';
 
 const CreationDateFilter = () => {
-	const creationDates = _.keys(useSelector((state) => state.chart.creationDates));
-	
 	const dispatch = useDispatch();
 
-	const handleDateFilter = (e) => {
+	const creationDates = _.keys(useSelector((state) => state.chart.creationDates));
+	
+	/*const filterByDate = (e) => {
 		const date = e.target.value;
 		dispatch(filterChartsByDate({date}));
-	}
+	}*/
 	
 	let dateFilter;
 	if (creationDates.length === 0) {
@@ -25,7 +25,7 @@ const CreationDateFilter = () => {
 			<div className="d-flex align-items-center">
 			<h6 className="m-2">Filter by date</h6>
 			<Form>
-				<Form.Select aria-label="Filter by Creation Date" onChange={handleDateFilter}>
+				<Form.Select aria-label="Filter by Creation Date" onChange={(e) => dispatch(filterChartsByDate(e.target.value))}>
 					<option>all charts</option>
 					{ creationDates.map((date, index) => <option key={index}>{date}</option>)}
 				</Form.Select>			
