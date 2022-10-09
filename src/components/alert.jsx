66830fbe1611b594c0e71eac79fdf9177db78592
Anton Krupin <krupin_anton@mail.ru>
2022-10-09 12:22:37 +0300

    remove import { useLocation } add location prop

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -1,18 +1,18 @@
-import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Alert } from 'react-bootstrap';
 
-const AlertMessage = () => {
+const AlertMessage = (props) => {
+	const { location } = props;
 	const charts = useSelector((state) => state.chart.charts);
-	const location = useLocation();
 	
 	let alertText = 'You have no Charts. Go to Settings, to Add Chart';
-	if (location.pathname === '/settings') {
+	if (location === '/settings') {
 		alertText = 'To add Chart - click button Add chart';
 	}
 
 	if (charts.length === 0) {
-		return ( <Alert className="text-center" variant="warning" >
+		return ( 
+		<Alert className="text-center" variant="warning" >
 			{alertText}
 		</Alert>
 		)
