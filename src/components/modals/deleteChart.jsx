d9309b53b4690debc45972e088466218b50b7bfa
Anton Krupin <krupin_anton@mail.ru>
2022-10-09 19:48:40 +0300

    change imports

--- src/components/modals/deleteChart.jsx
+++ src/components/modals/deleteChart.jsx
@@ -2,36 +2,37 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Modal, Button } from 'react-bootstrap';
 
-import { deleteChart, deleteDate } from '../../slices/chartReducer';
-import { deleteChartModalShow } from '../../slices/modalsReducer';
+import { deleteChart, deleteDate } from '../../slices/chartReducer.js';
+import { deleteChartModalShow } from '../../slices/modalsReducer.js';
 
 const DeleteModal = (props) => {
 	const dispatch = useDispatch();
   
 	const isDeleteChartModalShow = useSelector((state) => state.modals.isDeleteChartModalShow);
-  const chartForDelete = useSelector((state) => state.chart.chartForDeleteId);
+
+	const chartForDelete = useSelector((state) => state.chart.chartForDeleteId);
 
 	const handleDelete = () => {
-    dispatch(deleteDate(chartForDelete));
+		dispatch(deleteDate(chartForDelete));
 		dispatch(deleteChart(chartForDelete));
 		dispatch(deleteChartModalShow(props.id));
 	}
 
 	return (
-    <Modal show={isDeleteChartModalShow} onHide={() => dispatch(deleteChartModalShow())}>
-      <Modal.Header closeButton>
-        <Modal.Title>Delete Chart?</Modal.Title>
-      </Modal.Header>
-      <Modal.Footer>
-        <Button variant="info" onClick={() => dispatch(deleteChartModalShow())}>
+		<Modal show={isDeleteChartModalShow} onHide={() => dispatch(deleteChartModalShow())}>
+			<Modal.Header closeButton>
+				<Modal.Title>Delete Chart?</Modal.Title>
+			</Modal.Header>
+			<Modal.Footer>
+				<Button variant="info" onClick={() => dispatch(deleteChartModalShow())}>
           No
-        </Button>
-        <Button variant="danger" onClick={handleDelete}>
+				</Button>
+				<Button variant="danger" onClick={handleDelete}>
           Yes
-        </Button>
-      </Modal.Footer>
-  </Modal>
-  )
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	)
 };
 
 export default DeleteModal;
