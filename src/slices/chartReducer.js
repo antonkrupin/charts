44182cdfc3d3d0485710f2279e4f4fef9348ba79
antonkrupin <krupin_anton@mail.ru>
2022-10-10 15:11:49 +0300

    remove comments

--- src/slices/chartReducer.js
+++ src/slices/chartReducer.js
@@ -20,14 +20,13 @@ const chartSlice = createSlice({
 			const id = _.uniqueId(state.sessionPrefix);
 
 			const { title, type, series, date } = action.payload;
-			// const test = localStorage.getItem('charts');
-			/*if (!localStorage.getItem('sessionPrefix')) {
-				localStorage.setItem('sesionPrefix', id);
-			}*/
+
 			if (!localStorage.getItem('charts')) {
 				localStorage.setItem('charts', JSON.stringify([]));
 			}
+
 			const charts = JSON.parse(localStorage.getItem('charts'));
+
 			charts.unshift({
 				id,
 				date,
@@ -44,29 +43,15 @@ const chartSlice = createSlice({
 					series: series,
 				}
 			});
+
 			localStorage.setItem('charts', JSON.stringify(charts));
 			state.charts = JSON.parse(localStorage.getItem('charts'));
-			/*state.charts.unshift({
-				id,
-				date,
-				options: {
-					accessibility: {
-						enabled: false,
-					},
-					chart: {
-						type: type,
-					},
-					title: {
-						text: title,
-					},
-					series: series,
-				}
-			});*/
 		},
 		addDate: (state, action) => {
 			if (!localStorage.getItem('creationDates')) {
 				localStorage.setItem('creationDates', JSON.stringify({}));
 			}
+
 			const { date } = action.payload;
 			
 			const creationDates = JSON.parse(localStorage.getItem('creationDates'));
@@ -75,16 +60,13 @@ const chartSlice = createSlice({
 			} else {
 				creationDates[date] = 1;
 			}
+
 			localStorage.setItem('creationDates', JSON.stringify(creationDates));
 			state.creationDates = JSON.parse(localStorage.getItem('creationDates'));
-			/* if (date in state.creationDates) {
-				state.creationDates[date] += 1;
-			} else {
-				state.creationDates[date] = 1;
-			} */
 		},
 		filterChartsByDate: (state, action) => {
 			const date = action.payload;
+
 			state.chartsFilteredByDate = [];
 			state.charts.forEach((chart, index) => {
 				if (chart.date === date) {
@@ -94,7 +76,9 @@ const chartSlice = createSlice({
 		},
 		updateChart: (state, action) => {
 			const charts = JSON.parse(localStorage.getItem('charts'));
+
 			const { id, options } = action.payload;
+
 			charts.forEach((chart) => {
 				if (chart.id === id) {
 					chart.options = options;
@@ -102,31 +86,22 @@ const chartSlice = createSlice({
 			});
 			localStorage.setItem('charts', JSON.stringify(charts));
 			state.charts = JSON.parse(localStorage.getItem('charts'));
-
-			//state.creationDates = JSON.parse(localStorage.getItem('creationDates'));
-			/*state.charts.forEach((chart) => {
-				if (chart.id === id) {
-					chart.options = options;
-				}
-			});*/
 		},
 		deleteChart: (state, action) => {
 			const charts = JSON.parse(localStorage.getItem('charts'));
+
 			charts.forEach((chart, index) => {
 				if (chart.id === action.payload) {
 					charts.splice(index, 1);
 				}
 			});
+
 			localStorage.setItem('charts', JSON.stringify(charts));
 			state.charts = JSON.parse(localStorage.getItem('charts'))
-			/* state.charts.forEach((chart, index) => {
-				if (chart.id === action.payload) {
-					state.charts.splice(index, 1);
-				}
-			}); */
 		},
 		deleteDate: (state, action) => {
 			const id = action.payload;
+
 			let date;
 			state.charts.forEach((chart) => {
 				if (chart.id === id) {
@@ -134,24 +109,22 @@ const chartSlice = createSlice({
 				}
 			});
 
-			/* state.creationDates[date] -= 1;
-			if (state.creationDates[date] === 0) {
-				delete state.creationDates[date];
-			} */
-			// test
 			const creationDates = JSON.parse(localStorage.getItem('creationDates'));
+
 			creationDates[date] -= 1;
+
 			if (!creationDates[date]) {
 				delete creationDates[date];
 			}
+
 			localStorage.setItem('creationDates', JSON.stringify(creationDates));
 			state.creationDates = JSON.parse(localStorage.getItem('creationDates'));
-			// test
 		},
 	},
 	extraReducers: (builder) => {
 		builder.addCase(updateChartModalShow, (state, action) => {
 			const id = action.payload;
+
 			state.charts.forEach((chart) => {
 				if (chart.id === id) {
 					state.chartForChange = chart;
