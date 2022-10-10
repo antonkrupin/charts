26c5477d83136cea4acec241eedf5545aa978955
antonkrupin <krupin_anton@mail.ru>
2022-10-10 14:46:14 +0300

    add import navigationBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Nav } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import { Nav } from 'react-bootstrap';
+
 import ViewMode from './routes/viewMode.jsx';
 import Settings from './routes/settings.jsx';
 import Page404 from './routes/page404.jsx';
-import ClearLocalStorageBtn from './components/buttons/clearLocalStorageBtn.jsx';
+import NavigationBar from './components/navigationBar.jsx';
+//import ClearLocalStorageBtn from './components/buttons/clearLocalStorageBtn.jsx';
+
 import './App.css';
 
 const App = () => (
 	<BrowserRouter>
-		<Nav className="mt-2 mb-2 mr-auto justify-content-center" expand="lg">
+		{/*<Nav className="mt-2 mb-2 mr-auto justify-content-center" expand="lg">
 			<Nav.Item>
 				<Nav.Link as={Link} to="/viewmode">View Mode</Nav.Link>
 			</Nav.Item>
 			<Nav.Link as={Link} to="/settings">Settings</Nav.Link>
 			<ClearLocalStorageBtn />
-		</Nav>
+</Nav> */}
+		<NavigationBar />
 		<Routes>
 			<Route path="/" element={<ViewMode/>} />
 			<Route path="viewmode" element={<ViewMode />} />
