c3e07a90480259bad05a84f4c438952193694a39
antonkrupin <krupin_anton@mail.ru>
2022-10-10 13:58:50 +0300

    add clearLocalStorageBtn

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { Nav } from 'react-bootstrap';
 import ViewMode from './routes/viewMode.jsx';
 import Settings from './routes/settings.jsx';
 import Page404 from './routes/page404.jsx';
+import ClearLocalStorageBtn from './components/buttons/clearLocalStorageBtn.jsx';
 import './App.css';
 
 const App = () => (
 	<BrowserRouter>
-		<Nav className="mr-auto justify-content-center" expand="lg">
+		<Nav className="mt-2 mb-2 mr-auto justify-content-center" expand="lg">
 			<Nav.Item>
 				<Nav.Link as={Link} to="/viewmode">View Mode</Nav.Link>
 			</Nav.Item>
 			<Nav.Link as={Link} to="/settings">Settings</Nav.Link>
+			<ClearLocalStorageBtn />
 		</Nav>
 		<Routes>
 			<Route path="/" element={<ViewMode/>} />
